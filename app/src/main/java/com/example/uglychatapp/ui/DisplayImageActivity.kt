8d15dcc50f17a3998725a298060eb6916e81d317
pranav0281999 <pranav0281999@gmail.com>
2020-06-21 18:59:56 +0530

    Updates to upload image on button click

--- app/src/main/java/com/example/uglychatapp/ui/DisplayImageActivity.kt
+++ app/src/main/java/com/example/uglychatapp/ui/DisplayImageActivity.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.uglychatapp.MainApplication
 import com.example.uglychatapp.R
@@ -16,54 +18,68 @@ import java.util.*
 
 class DisplayImageActivity : AppCompatActivity() {
     var imageView: ImageView? = null
+    var btnSend: Button? = null
+    var imagePath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_image)
+
         imageView = findViewById(R.id.display_image_iv)
+        btnSend = findViewById(R.id.display_image_btn_send)
 
         if (intent.hasExtra("imagePath")) {
-            val imagePath = intent.getStringExtra("imagePath")
+            imagePath = intent.getStringExtra("imagePath")
 
             imageView?.setImageURI(Uri.parse(imagePath))
+        }
 
-            val uuid = UUID.randomUUID().toString()
-
-            try {
-                val multipartUploadRequest = MultipartUploadRequest(this, serverUrl = "http://" + MainApplication.serverNode + ":" + MainApplication.serverNodePort + "/chat/image")
-                        .setMethod("POST")
-                        .addHeader("filename", uuid)
-                        .addFileToUpload(
-                                filePath = imagePath,
-                                parameterName = "image"
-                        )
-
-                multipartUploadRequest.subscribe(this, this, object : RequestObserverDelegate {
-                    override fun onProgress(context: Context, uploadInfo: UploadInfo) {
-                        Log.v(TAG, uploadInfo.uploadedBytes.toString())
-                    }
-
-                    override fun onSuccess(context: Context, uploadInfo: UploadInfo, serverResponse: ServerResponse) {
-                        Log.v(TAG, uploadInfo.uploadedBytes.toString())
-                    }
-
-                    override fun onError(context: Context, uploadInfo: UploadInfo, exception: Throwable) {
-                        Log.v(TAG, exception.toString())
-                    }
-
-                    override fun onCompleted(context: Context, uploadInfo: UploadInfo) {
-                        Log.v(TAG, uploadInfo.uploadedBytes.toString())
-                    }
-
-                    override fun onCompletedWhileNotObserving() {
-                        Log.v(TAG, "onCompletedWhileNotObserving")
-                    }
-                })
-
-                multipartUploadRequest.startUpload()
-            } catch (e: Exception) {
-                e.printStackTrace()
+        btnSend?.setOnClickListener {
+            if (imagePath != null) {
+                sendImage()
+            } else {
+                Toast.makeText(this, "Couldn't send image", Toast.LENGTH_LONG).show()
             }
+        }
+    }
+
+    private fun sendImage() {
+        val uuid = UUID.randomUUID().toString()
+
+        try {
+            val multipartUploadRequest = MultipartUploadRequest(this, serverUrl = "http://" + MainApplication.serverNode + ":" + MainApplication.serverNodePort + "/chat/image")
+                    .setMethod("POST")
+                    .addHeader("filename", uuid)
+                    .addFileToUpload(
+                            filePath = imagePath!!,
+                            parameterName = "image"
+                    )
+
+            multipartUploadRequest.subscribe(this, this, object : RequestObserverDelegate {
+                override fun onProgress(context: Context, uploadInfo: UploadInfo) {
+                    Log.v(TAG, uploadInfo.uploadedBytes.toString())
+                }
+
+                override fun onSuccess(context: Context, uploadInfo: UploadInfo, serverResponse: ServerResponse) {
+                    finish()
+                }
+
+                override fun onError(context: Context, uploadInfo: UploadInfo, exception: Throwable) {
+                    Toast.makeText(context, "Couldn't send image", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onCompleted(context: Context, uploadInfo: UploadInfo) {
+                    Log.v(TAG, uploadInfo.uploadedBytes.toString())
+                }
+
+                override fun onCompletedWhileNotObserving() {
+                    Log.v(TAG, "onCompletedWhileNotObserving")
+                }
+            })
+
+            multipartUploadRequest.startUpload()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
